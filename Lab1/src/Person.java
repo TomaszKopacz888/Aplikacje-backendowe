fd2608e1ae5ffa071799dc9d66c07d2d68cf4178
TomaszKopacz888 <t.kopacz20@wp.pl>
2022-03-23 11:07:24 +0100

    zad8 finished

--- Lab1/src/Person.java
+++ Lab1/src/Person.java
@@ -1,8 +1,21 @@
 public class Person {
     public String name;
     public int age;
+
+    public Person(){
+
+    }
+
     public Person(String name, int age){
         this.name=name;
         this.age=age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
--- Lab1/src/Zad8.java
+++ Lab1/src/Zad8.java
@@ -1,10 +1,16 @@
-import scala.util.parsing.json.JSON;
-import scala.util.parsing.json.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 
 public class Zad8 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)  throws IOException{
+        ObjectMapper objectMapper=new ObjectMapper();
+        //Person tomasz=new Person("Tomasz", 31);
+        String personJSON="{\"name\":\"Tomasz\",\"age\":31}";
+        Person tomasz=objectMapper.readValue(personJSON, Person.class);
+        //System.out.println(personJSON);
+        System.out.println(tomasz.getName());
+        System.out.println(tomasz.getAge());
     }
 }
 
