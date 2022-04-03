764ee0d96e8c401d71ef351ab2c3647fde873353
TomaszKopacz888 <t.kopacz20@wp.pl>
2022-04-03 18:30:03 +0200

    Finished

--- Lab2/src/main/java/com/example/demo/controllers/UserController.java
+++ Lab2/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import com.example.demo.UserEntity;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,18 +17,9 @@ import java.util.Map;
 @Controller
 public class UserController {
 
-    //private List<Integer ,UserEntity> users;
-
     @Autowired
     private UsersService usersService;
 
-    public UserController() {
-        //this.users=new ArrayList<>();
-        this.usersService.addUser(21, "John");
-        this.usersService.addUser(32, "Mike");
-        this.usersService.addUser(21, "Scott");
-        this.usersService.addUser(35, "Chris");
-    }
 
     @RequestMapping("/users")
     @ResponseBody
@@ -43,15 +35,21 @@ public class UserController {
         return this.usersService.getUserById(id);
     }
 
+    @RequestMapping("/users/{id}/remove")
+    @ResponseBody
+    public Object removeUsers(
+            @PathVariable int id
+    ) {
+        String userName=this.usersService.removeUserById(id);
+        return "Removed user "+userName;
+    }
+
     @RequestMapping("/users/add")
     @ResponseBody
-    public String addUsers(
+    public Object addUsers(
             @RequestParam String name,
             @RequestParam Integer age
     ) {
-        UserEntity user = new UserEntity(age, name);
-        //users.add(user);
-
-        return "Added new user";
+        return this.usersService.addUser(age, name);
     }
 }
--- Lab2/src/main/java/com/example/demo/controllers/UsersService.java
+++ Lab2/src/main/java/com/example/demo/controllers/UsersService.java
@@ -3,27 +3,47 @@ package com.example.demo.controllers;
 import com.example.demo.UserEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class UsersService {
-    private Map<Integer, UserEntity> users;
-    private int lastId=0;
-    public UsersService(){
-        this.users=new HashMap<>();
+    private final Map<Integer, UserEntity> users;
+    private int lastId;
+
+    public UsersService() {
+        this.users = new HashMap<>();
+        this.users.put(0, new UserEntity(21, "John"));
+        this.users.put(1, new UserEntity(32, "Mike"));
+        this.users.put(2, new UserEntity(21, "Scott"));
+        this.users.put(3, new UserEntity(35, "Chris"));
+        this.lastId = 4;
     }
 
-    public Object getAllUsers(){
-        return this.users;
+    public Object getAllUsers() {
+        return this.users.values();
     }
+
     public Object getUserById(int id) {
         if (this.users.containsKey(id)) return this.users.get(id);
         else return "There is no user id " + id;
+    }
+
+    public String removeUserById(int id) {
+        if (this.users.containsKey(id)) {
+            UserEntity user = users.get(id);
+            this.users.remove(id);
+            return user.getName();
+        }
+        else return "There is no user id " + id;
 
     }
-    public void addUser(int age, String name){
-        this.users.put(this.lastId, new UserEntity(age, name));
+
+    public UserEntity addUser(int age, String name) {
+        UserEntity user = new UserEntity(age, name);
+        this.users.put(this.lastId, user);
         this.lastId++;
+        return user;
     }
 }
